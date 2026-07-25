e9f62c82963296c48934a026ddad776767ebb5f0
Lu Fengqi <curiouslfq@gmail.com>
2026-07-26 00:25:29 +0800

    chore: remove vestigial return {} from mappings.lua
    Side-effect script, not a module. No consumer uses this return.

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -54,5 +54,3 @@ vim.keymap.set('t', '<C-h>', '<C-\\><C-n><C-w>h')
 vim.keymap.set('t', '<C-j>', '<C-\\><C-n><C-w>j')
 vim.keymap.set('t', '<C-k>', '<C-\\><C-n><C-w>k')
 vim.keymap.set('t', '<C-l>', '<C-\\><C-n><C-w>l')
-
-return {}
